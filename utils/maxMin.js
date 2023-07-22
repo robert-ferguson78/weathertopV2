14cf37b52e62e65303d5b61a024d0a817668b57a
Robert Ferguson <robertferguson78@gmail.com>
2023-07-22 23:50:50 +0100

    minMax code refactor

--- utils/maxMin.js
+++ utils/maxMin.js
@@ -1,30 +1,21 @@
 export const maxMin = {
     minMaxReadings(stationReadings, readingType, minMax) {
+      let minVal = null;
+      let maxVal = null;
         if (readingType === "temperature") {
-          let minTemp = Math.min(...stationReadings.map((stationReadings) => stationReadings.temperature));
-          let maxTemp = Math.max(...stationReadings.map((stationReadings) => stationReadings.temperature));
-      
-          if (minMax === "min") {
-            return minTemp;
-          } else if (minMax === "max") {
-            return maxTemp;
-          }
+          minVal = Math.min(...stationReadings.map((stationReadings) => stationReadings.temperature));
+          maxVal = Math.max(...stationReadings.map((stationReadings) => stationReadings.temperature));
         } else if (readingType === "windSpeed") {
-          let minWindSpeed = Math.min(...stationReadings.map((stationReadings) => stationReadings.windSpeed));
-          let maxWindSpeed = Math.max(...stationReadings.map((stationReadings) => stationReadings.windSpeed));
-          if (minMax === "min") {
-            return minWindSpeed;
-          } else if (minMax === "max") {
-            return maxWindSpeed;
-          }
+          minVal = Math.min(...stationReadings.map((stationReadings) => stationReadings.windSpeed));
+          maxVal = Math.max(...stationReadings.map((stationReadings) => stationReadings.windSpeed));
         } else if (readingType === "pressure") {
-          let minPressure = Math.min(...stationReadings.map((stationReadings) => stationReadings.pressure));
-          let maxPressure = Math.max(...stationReadings.map((stationReadings) => stationReadings.pressure));
-          if (minMax === "min") {
-            return minPressure;
-          } else if (minMax === "max") {
-            return maxPressure;
-          }
+          minVal = Math.min(...stationReadings.map((stationReadings) => stationReadings.pressure));
+          maxVal = Math.max(...stationReadings.map((stationReadings) => stationReadings.pressure));
+        }
+        if (minMax === "min") {
+          return minVal;
+        } else if (minMax === "max") {
+          return maxVal;
         }
       }
 };
