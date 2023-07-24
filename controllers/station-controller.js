e4d4d5bd5e42c4a981335df64f259b693a20a362
Robert Ferguson <robertferguson78@gmail.com>
2023-07-24 12:41:59 +0100

    Added date and time stamp with format yyyy-mn-dd-hr-mn-ss plus set display

--- controllers/station-controller.js
+++ controllers/station-controller.js
@@ -20,7 +20,29 @@ export const stationController = {
   },
   async addReading(request, response) {
     const station = await stationStore.getStationById(request.params.id);
+
+    // Added date with if statements so dates have 2 digitis if single nunber is returned
+    const date = new Date();
+    let yyyy = date.getFullYear();
+    let mn = date.getMonth() + 1;
+    let dd = date.getDate();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    if(mn<10)
+        mn = '0' + mn;
+    if(dd<10)
+        dd = '0' + dd;
+    if(hh<10)
+        hh = '0' + hh;
+    if(mm<10)
+        mm = '0' + mm;
+    if(ss<10)
+        ss = '0' + ss;
+    let dateTime = yyyy + "-" + mn + "-" + dd + " " + hh + ":" + mm + ":" + ss;
+    
     const newReading = {
+      timeStamp: String(dateTime),
       code: Number(request.body.code),
       temperature: Number(request.body.temperature),
       windDirection: Number(request.body.windDirection),
