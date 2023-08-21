55f690c99223f28dc12dbf7d9c36f54f48cb92b4
Robert Ferguson <robertferguson78@gmail.com>
2023-08-21 17:06:57 +0100

    Add Dashboard Controoler comments and Capitalise station Name first letter

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -3,38 +3,50 @@ import { stationStore } from "../models/station-store.js";
 import { lastReadings } from "../utils/analytics.js";
 
 export const dashboardController = {
+  // Initial index page to display with view data to pass in
   async index(request, response) {
     const loggedInUser = await accountsController.getLoggedInUser(request);
+
+    // Data to pass into View
     const viewData = {
       title: "Station Dashboard",
       stations: await stationStore.getStationsByUserId(loggedInUser._id),
     };
+
+    // Loop through last reading in Utils/Analytics to diplay last readings based on stations in view data
     for (const station of viewData.stations) {
       const readingObject = await lastReadings(station._id);
-      // console.log(`${JSON.stringify(readingObject)}`);
+      // Add the Analytics reading to this viewData
       Object.assign(station, readingObject.reading)
     }
+
     console.log("dashboard rendering");
-    // console.log(`${JSON.stringify(viewData)}`);
     response.render("dashboard-view", viewData);
   },
 
+  // Add station to the logged in user 
   async addStation(request, response) {
     const loggedInUser = await accountsController.getLoggedInUser(request);
+    const mixedCapStationname = request.body.stationName;
+    const capitaliseName = mixedCapStationname.charAt(0).toUpperCase() + mixedCapStationname.slice(1);
+    // Assign the data to be saved in storage
     const newStation = {
-      stationName: request.body.stationName,
+      stationName: capitaliseName,
       latitude: request.body.latitude,
       longitude: request.body.longitude,
       userid: loggedInUser._id,
     };
     console.log(`adding station ${newStation.stationName}`);
+    // Save data in station storage
     await stationStore.addStation(newStation);
     response.redirect("/dashboard");
   },
 
+  // Delete station
   async deleteStation(request, response) {
     const stationId = request.params.id;
     console.log(`Deleting Station ${stationId}`);
+    // Delete Station and associated Readings
     await stationStore.deleteStationById(stationId);
     response.redirect("/dashboard");
   },
