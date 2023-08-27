98456bd97c0d295815985859b797d1308c0f73b7
Robert Ferguson <robertferguson78@gmail.com>
2023-08-27 19:58:22 +0100

    console log update for users details

--- middleware/check-user-auth.js
+++ middleware/check-user-auth.js
@@ -1,7 +1,7 @@
 // Check for if the user is logged in
 export const checkUserAuth = (redirectTo = "/login") => {
     return async (request, response, next) => {
-        console.log(`Restricted area: ${JSON.stringify(request.user)}`);
+        console.log(`User details: ${JSON.stringify(request.user)}`);
         // redirect to login page if no user is detected
         if(!request.user) {
             response.redirect(redirectTo);
